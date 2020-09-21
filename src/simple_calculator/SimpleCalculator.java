a732e5d46ede52ea427244ed5270a54950020d9d
tungchihuy95 <tungjpvn95@gmail.com>
2020-09-21 08:55:10 +0700

    calculator

---
+++ src/simple_calculator/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package simple_calculator;
+
+public class SimpleCalculator {
+    public static int add(int first, int second){
+        throw new UnsupportedOperationException();
+    }
+
+    public static int sub(int first, int second){
+        throw new UnsupportedOperationException();
+    }
+}
+
+
